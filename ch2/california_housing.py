4e3dd772b895a561857fe22afafa80a9660d3d8b
Hernan Razo <hrazo@hawk.iit.edu>
2019-06-27 23:20:13 -0500

    added data splitting

--- ch2/california_housing.py
+++ ch2/california_housing.py
@@ -2,25 +2,36 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import hashlib
+from sklearn.model_selection import train_test_split
 
 data = pd.read_csv('housing.csv')
 
 
 print(data.info())
 
-def split_dataset(data, ratio):
+def test_set_check(identifier, ratio, hash):
 
-    shuffled_indices = np.random.permutation(len(data))
-    test_size = int(len(data) * ratio)
-    test_indices = shuffled_indices[:test_size]
-    train_indices = shuffled_indices[test_size:]
+    hash_value = hash(np.int64(identifier)).digest()[-1] < 256 * ratio
 
-    test_data = data.iloc[test_indices]
-    train_data = data.iloc[train_indices]
-
-    return test_data, train_data
+    return hash_value
 
 
-train_set, test_set = split_dataset(data, 0.2)
+def split_dataset(data, ratio, id_col, hash = hashlib.md5):
+
+    ids = data[id_col]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, ratio, hash))
+    new_data = data.loc[~in_test_set], data.loc[in_test_set]
+
+    return new_data
+
+housing_with_id = data.reset_index()
+train_set, test_set = split_dataset(housing_with_id, 0.2, 'index')
+
+housing_with_id["id"] = data["longitude"] * 1000 + data["latitude"]
+train_set, test_set = split_dataset(housing_with_id, 0.2, "id")
+
+train_set, test_set = train_test_split(data, test_size = 0.2, random_state = 42)
+
 print("test: " + str(len(test_set)))
 print("train: " + str(len(train_set)))
