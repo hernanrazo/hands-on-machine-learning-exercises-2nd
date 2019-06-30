7de3b12fe23667a92de62be88ae971a9569380d4
Hernan Razo <hrazo@hawk.iit.edu>
2019-06-30 18:00:15 -0500

    added to california_housing.py

--- ch2/california_housing.py
+++ ch2/california_housing.py
@@ -4,11 +4,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import hashlib
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
 
 data = pd.read_csv('housing.csv')
 
-
+print(' ')
+print('printing data info:')
 print(data.info())
+print('=================================================================')
+
 
 def test_set_check(identifier, ratio, hash):
 
@@ -25,7 +29,19 @@ def split_dataset(data, ratio, id_col, hash = hashlib.md5):
 
     return new_data
 
+def income_cat_proportions(data_set):
+
+    income_cat = data_set['income_cat'].value_counts() / len(data_set)
+
+    return income_cat
+
+
 housing_with_id = data.reset_index()
+print(' ')
+print('new dataframe:')
+print(housing_with_id.head())
+print('=================================================================')
+
 train_set, test_set = split_dataset(housing_with_id, 0.2, 'index')
 
 housing_with_id["id"] = data["longitude"] * 1000 + data["latitude"]
@@ -33,5 +49,81 @@ train_set, test_set = split_dataset(housing_with_id, 0.2, "id")
 
 train_set, test_set = train_test_split(data, test_size = 0.2, random_state = 42)
 
+
+print(' ')
+print('Printing testing and training set sizes:')
 print("test: " + str(len(test_set)))
 print("train: " + str(len(train_set)))
+print('=================================================================')
+
+
+
+data['income_cat'] = np.ceil(data['median_income'] / 1.5)
+data['income_cat'].where(data['income_cat'] < 5, 5.0, inplace = True)
+
+income_split = StratifiedShuffleSplit(n_splits = 1, test_size = 0.2, random_state = 42)
+
+for train_index, test_index in income_split.split(data, data['income_cat']):
+
+    strat_train_set = data.loc[train_index]
+    strat_test_set = data.loc[test_index]
+
+
+train_set, test_set = train_test_split(data, test_size = 0.2, random_state = 42)
+
+compare_proportions = pd.DataFrame({'Overall': income_cat_proportions(data),
+    'Stratified': income_cat_proportions(strat_test_set),
+    'Random': income_cat_proportions(test_set),}).sort_index()
+
+compare_proportions['Rand. %Error'] = 100 * compare_proportions['Random'] / compare_proportions['Overall'] - 100
+compare_proportions['Strat. %Error'] = 100 * compare_proportions['Stratified'] / compare_proportions['Overall'] - 100
+
+print(' ')
+print('comparing proportions:')
+print(compare_proportions)
+print('=================================================================')
+
+
+for set in (strat_train_set, strat_test_set):
+
+    set.drop(['income_cat'], axis = 1, inplace = True)
+
+#data cleaning
+
+print('starting data cleaning process...')
+
+housing = strat_train_set.drop('median_house_value', axis = 1)
+housing_labels = strat_train_set['median_house_value'].copy()
+
+sample_incomplete_rows = housing[housing.isnull().any(axis = 1)].head()
+
+median = housing['total_bedrooms'].median()
+sample_incomplete_rows['total_bedrooms'].fillna(median, inplace = True)
+
+try:
+    from sklearn.impute import SimpleImputer
+except ImportError:
+    from sklearn.preprocessing import Imputer as SimpleImputer
+
+imputer = SimpleImputer(strategy = 'median')
+
+housing_num = housing.drop('ocean_proximity', axis = 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
